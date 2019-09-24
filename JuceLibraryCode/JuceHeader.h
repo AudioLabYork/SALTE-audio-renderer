16ac1e3737f5bf6290b4d5e07b6d6a71fa3e2689
Tomasz Rudzki <tr837@york.ac.uk>
2019-09-24 17:00:34 +0100

    Juce project renamed

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "saltf";
+    const char* const  projectName    = "SALTE";
     const char* const  companyName    = "University of York";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
